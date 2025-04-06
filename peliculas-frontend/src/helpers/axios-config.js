8cffbdc6f28c76f680c37eb3f93b33b22455beb5
LucasRD26 <lucas.rodriguez@est.iudigital.edu.co>
2025-04-06 10:02:52 -0500

    update axios

--- peliculas-frontend/src/helpers/axios-config.js
+++ peliculas-frontend/src/helpers/axios-config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000/'
+    baseURL: 'https://app-pelicula-1.onrender.com/media'
 })
 
 export {axiosInstance }
